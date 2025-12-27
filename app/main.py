4f87febac9b04ef210dbe6a9224bd350793fb17b
Vasyl Cherkes <cherkesva@gmail.com>
2025-12-27 15:13:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
@@ -5,40 +6,58 @@ from iot.message import Message, MessageType
 from iot.service import IOTService
 
 
-def main() -> None:
+async def main() -> None:
     # create an IOT service
     service = IOTService()
 
     # create and register a few devices
-    hue_light = HueLightDevice()
-    speaker = SmartSpeakerDevice()
-    toilet = SmartToiletDevice()
-    hue_light_id = service.register_device(hue_light)
-    speaker_id = service.register_device(speaker)
-    toilet_id = service.register_device(toilet)
 
-    # create a few programs
-    wake_up_program = [
-        Message(hue_light_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
+    devices = [
+        HueLightDevice(),
+        SmartSpeakerDevice(),
+        SmartToiletDevice()
     ]
 
-    sleep_program = [
+    device_ids = await asyncio.gather(
+        *[service.register_device(device) for device in devices]
+    )
+
+    hue_light_id, speaker_id, toilet_id = device_ids
+
+    # create a few programs
+    wake_up_parallel = [
+        Message(hue_light_id, MessageType.SWITCH_ON),
+        Message(speaker_id, MessageType.SWITCH_ON),
+    ]
+
+    wake_up_sequence = [
+        Message(
+            speaker_id,
+            MessageType.PLAY_SONG,
+            "Rick Astley - Never Gonna Give You Up"
+        ),
+    ]
+
+    sleep_parallel = [
         Message(hue_light_id, MessageType.SWITCH_OFF),
         Message(speaker_id, MessageType.SWITCH_OFF),
+    ]
+
+    sleep_sequence = [
         Message(toilet_id, MessageType.FLUSH),
         Message(toilet_id, MessageType.CLEAN),
     ]
 
     # run the programs
-    service.run_program(wake_up_program)
-    service.run_program(sleep_program)
+    await service.run_parallel(wake_up_parallel)
+    await service.run_sequence(wake_up_sequence)
 
+    await service.run_parallel(sleep_parallel)
+    await service.run_sequence(sleep_sequence)
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main()
+    asyncio.run(main())
     end = time.perf_counter()
 
     print("Elapsed:", end - start)
